d738c268a3e77d91c73ac38b2f9caec3ab0146c5
Shahzain <6561371+shahzainb@users.noreply.github.com>
2020-03-12 10:17:50 +0000

    Fixed examples

--- examples/run.js
+++ examples/run.js
@@ -1,33 +1,41 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 main();
 
 function main() {
-  console.log('Starting building examples');
+  console.log("Starting building examples");
 
-  webpack({
-    entry: path.resolve(__dirname, 'app'),
-    output: {
-      path: path.resolve(__dirname, 'output'),
-      filename: 'bundle.js',
+  webpack(
+    {
+      entry: path.resolve(__dirname, "app"),
+      output: {
+        path: path.resolve(__dirname, "output"),
+        filename: "bundle.js"
+      },
+      resolve: {
+        extensions: [".js", ".md"]
+      },
+      module: {
+        rules: [
+          {
+            test: /\.md$/,
+            use: [
+              "html-loader",
+              path.resolve(__dirname, "../", "index"),
+              "markdown-loader"
+            ]
+          }
+        ]
+      }
     },
-    resolve: {
-      extensions: ['.js', '.md'],
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.md$/,
-          loader: 'html!../index!markdown-loader',
-        }
-      ]
+    (err, stats) => {
+      if (err || stats.hasErrors()) {
+        // console.log(err);
+        console.log(stats.compilation.errors);
+      } else {
+        console.log("Finished. See /output");
+      }
     }
-  }, function(err) {
-    if(err) {
-        console.error(err);
-    }
-
-    console.log('Finished. See /output');
-  });
-};
+  );
+}
